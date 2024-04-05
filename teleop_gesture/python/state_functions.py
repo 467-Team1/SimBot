9b95586483ad1a1f3e251c9c523cefbcf69c9c84
{{cpgvega}} <{{cpvega@umich.edu}}>
2024-04-05 12:46:32 -0400

    Added new gesture-test branch & debugging teleop (DIRECTION WORKS BUT NOT VELOCITY)

--- teleop_gesture/python/state_functions.py
+++ teleop_gesture/python/state_functions.py
@@ -1,6 +1,7 @@
 from enum import Enum
 import numpy as np
 import pygame
+import time
 
 class State(Enum):
     FORWARD = 1
@@ -19,7 +20,7 @@ def slow_down(cur_motor_command, current_state):
         if cur_motor_command.trans_v <= 0.05:
             cur_motor_command.trans_v = 0.05
 
-        cur_motor_command.trans_v -= step
+        cur_motor_command.trans_v = -step
 
     # Increase the speed until it reaches -0.05 m/s
     elif current_state == State.BACKWARD:
@@ -27,7 +28,7 @@ def slow_down(cur_motor_command, current_state):
         if cur_motor_command.trans_v <= -0.05:
             cur_motor_command.trans_v = -0.05
 
-        cur_motor_command.trans_v += step
+        cur_motor_command.trans_v = step
 
     # Decrease the angular velocity until it reaches 0.05 rad/s
     elif current_state == State.LEFT:
@@ -35,7 +36,7 @@ def slow_down(cur_motor_command, current_state):
         if cur_motor_command.angular_v <= 0.05:
             cur_motor_command.angular_v = 0.05
 
-        cur_motor_command.angular_v -= step
+        cur_motor_command.angular_v = -step
 
     # Increase the angular velocity until it reaches -0.05 rad/s
     elif current_state == State.RIGHT:   
@@ -43,87 +44,97 @@ def slow_down(cur_motor_command, current_state):
         if cur_motor_command.angular_v <= -0.05:
             cur_motor_command.angular_v = -0.05
 
-        cur_motor_command.angular_v += step
+        cur_motor_command.angular_v = step
 
 # Speed Up - accelerates to 0.5 m/s, 1.0 rad/s or -0.5 m/s, -1.0 rad/s
-def speed_up(cur_motor_command, current_state):
+def speed_up(cur_motor_command, current_state, key_input):
     step = 0.05
     print("Accelerating...")
     # Increase the speed until it reaches 0.5 m/s
     if current_state == State.FORWARD:
         # Check if the robot is the robot reached the minimum speed
-        if cur_motor_command.trans_v >= 0.5:
-            cur_motor_command.trans_v = 0.5
+        while(cur_motor_command.trans_v < 0.5):
+            time.sleep(0.1)
+            cur_motor_command.trans_v = step
+            step += 0.001
 
-        cur_motor_command.trans_v += step
+            if (not key_input[pygame.K_UP]):
+                break
 
     # Increase the speed until it reaches -0.5 m/s
     elif current_state == State.BACKWARD:
         # Check if the robot is the robot reached the minimum speed
-        if cur_motor_command.trans_v <= -0.5:
-            cur_motor_command.trans_v = -0.5
-            
+        while(cur_motor_command.trans_v > -0.5):
+            time.sleep(0.1)
+            cur_motor_command.trans_v = -step
+            step -= 0.001
 
-        cur_motor_command.trans_v -= step
+            if (not key_input[pygame.K_DOWN]):
+                break
 
     # Increase the angular velocity until it reaches 1.0 rad/s
     elif current_state == State.LEFT:
         # Check if the robot is the robot reached the minimum speed
         if cur_motor_command.angular_v >= 1.0:
             cur_motor_command.angular_v = 1.0
-            
 
-        cur_motor_command.angular_v += step
+        cur_motor_command.angular_v = step
 
     # Increase the angular velocity until it reaches -0.05 rad/s
     elif current_state == State.RIGHT:   
         # Check if the robot is the robot reached the minimum speed
         if cur_motor_command.angular_v <= -1.0:
             cur_motor_command.angular_v = -1.0
-            
 
-        cur_motor_command.angular_v -= step
+        cur_motor_command.angular_v = -step
 
 # Stop - decelerates to 0.0 m/s, 0.0 rad/s
 def stop(cur_motor_command, current_state):
     step = 0.05
     if current_state == State.FORWARD:
         while cur_motor_command.trans_v > 0:
-            cur_motor_command.trans_v -= step
+            cur_motor_command.trans_v = -step
 
     elif current_state == State.BACKWARD:
         while cur_motor_command.trans_v < 0:
-            cur_motor_command.trans_v += step
+            cur_motor_command.trans_v = step
 
     elif current_state == State.LEFT:
         while cur_motor_command.angular_v > 0:
-            cur_motor_command.angular_v -= step
+            cur_motor_command.angular_v = -step
 
     elif current_state == State.RIGHT:
         while cur_motor_command.angular_v < 0:
-            cur_motor_command.angular_v += step
+            cur_motor_command.angular_v = step
 
 # Going Forward - climbs up to 0.25 m/s or stays at 0.25 m/s
 def forward(cur_motor_command, key_input): #, left_hand_gesture):
+    
+    # Zero Out Angular Velocity
+    cur_motor_command.angular_v = 0.0
 
     # Check if the left hand gesture is "SpeedUp", "SlowDown", "Continue", or "Stop"
     if key_input[pygame.K_w]: #left_hand_gesture == "SpeedUp":
-        speed_up(cur_motor_command, State.FORWARD)
+        speed_up(cur_motor_command, State.FORWARD, key_input)
     if key_input[pygame.K_s]: #left_hand_gesture == "SlowDown":
         slow_down(cur_motor_command, State.FORWARD)
     if key_input[pygame.K_SPACE]: #left_hand_gesture == "Stop":
         stop(cur_motor_command, State.FORWARD)
     else:
         # Check if the robot is already moving forward
-        if cur_motor_command.trans_v >= 0.0:
+        if cur_motor_command.trans_v > 0.0:
             return State.FORWARD
 
         # Slowly Accelrate to 0.25 m/s - Change '10' to '>10' to control the speed of acceleration
         for step in np.linspace(0, 0.25, 10):
-            cur_motor_command.trans_v += step
+            print("Getting to Speed...")
+            cur_motor_command.trans_v = step
 
 # Going Backward - climbs down to -0.25 m/s or stays at -0.25 m/s
 def backward(cur_motor_command, key_input): #, left_hand_gesture
+     
+    # Zero Out Angular Velocity
+    cur_motor_command.angular_v = 0.0
 
     # Check if the left hand gesture is "SpeedUp", "SlowDown", "Continue", or "Stop"
     if key_input[pygame.K_w]: #left_hand_gesture == "SpeedUp":
@@ -134,15 +145,19 @@ def backward(cur_motor_command, key_input): #, left_hand_gesture
         stop(cur_motor_command, State.BACKWARD)
     else:
         # Check if the robot is already moving backward
-        if cur_motor_command.trans_v <= 0.0:
+        if cur_motor_command.trans_v < 0.0:
             return State.BACKWARD
         
         # Slowly Decelerate to -0.25 m/s - Change '10' to '>10' to control the speed of deceleration
         for step in np.linspace(0, 0.25, 10):
-            cur_motor_command.trans_v -= step
+            cur_motor_command.trans_v = -step
 
 # Going Left - turns left at 0.5 rad/s
 def left(cur_motor_command, key_input): #, left_hand_gesture
+
+    # Zero out Trans Velocity
+    cur_motor_command.trans_v = 0.0
+
     # Check if the left hand gesture is "SpeedUp", "SlowDown", "Continue", or "Stop"
     if key_input[pygame.K_w]: #left_hand_gesture == "SpeedUp":
         speed_up(cur_motor_command, State.LEFT)
@@ -152,7 +167,7 @@ def left(cur_motor_command, key_input): #, left_hand_gesture
         stop(cur_motor_command, State.LEFT)
     else:
         # Check if the robot is already turning left
-        if cur_motor_command.angular_v >= 0.0:
+        if cur_motor_command.angular_v > 0.0:
             return State.LEFT
 
         # Slowly Decelerate to 0.0 m/s - Change '10' to '>10' to control the speed of deceleration
@@ -161,6 +176,10 @@ def left(cur_motor_command, key_input): #, left_hand_gesture
 
 # Going Right - turns right at -0.5 rad/s
 def right(cur_motor_command, key_input): #, left_hand_gesture
+
+    # Zero out Trans Velocity
+    cur_motor_command.trans_v = 0.0
+
     # Check if the left hand gesture is "SpeedUp", "SlowDown", "Continue", or "Stop"
     if key_input[pygame.K_w]: #left_hand_gesture == "SpeedUp":
         speed_up(cur_motor_command, State.RIGHT)
@@ -170,7 +189,7 @@ def right(cur_motor_command, key_input): #, left_hand_gesture
         stop(cur_motor_command, State.RIGHT)
     else:
         # Check if the robot is already turning right
-        if cur_motor_command.angular_v <= 0.0:
+        if cur_motor_command.angular_v < 0.0:
             return State.RIGHT
 
         # Slowly Decelerate to 0.0 m/s - Change '10' to '>10' to control the speed of deceleration
