d16f92c00023d51109b1773c62819ed2f8940b80
{cpgvega} <{cpvega@umich.edu}>
2024-04-23 18:37:02 -0400

    Claw Feature working | getting a buzzing noise from the servo [please look into further]

--- teleop_gesture/python/state_functions.py
+++ teleop_gesture/python/state_functions.py
@@ -6,13 +6,14 @@ import numpy as np
 import RPi.GPIO as GPIO
 from time import sleep
 
-
 class State(Enum):
     FORWARD = 1
     BACKWARD = 2
     LEFT = 3
     RIGHT = 4
-    STAND_BY = 5
+    CLAW_OPEN = 5
+    CLAW_CLOSE = 6
+    STAND_BY = 7
 
 ### VELOCITY CHANGE FUNCTIONS ### 
 
@@ -21,7 +22,7 @@ def slow_down(cur_motor_command, current_state):
     step = 0.02
     trans_speed_cap = 0.20
 
-    print("Decelerating...")
+    print("Decelerating...from", current_state)
     # # Decrease the speed until it reaches 0.2 m/s
     if current_state == State.FORWARD:
         # Check if the robot is the robot reached the minimum speed
@@ -41,7 +42,7 @@ def speed_up(cur_motor_command, current_state):
     step = 0.02
     trans_speed_cap = 0.40
 
-    print("Accelerating...")
+    print("Accelerating...from", current_state)
     # # Increase the speed until it reaches 0.4 m/s
     if current_state == State.FORWARD:
         # Check if the robot is the robot reached the minimum speed
@@ -82,7 +83,7 @@ def forward(cur_motor_command, left_hand_gesture):
         if cur_motor_command.trans_v > 0.0:
             return State.FORWARD
 
-        # Slowly Accelrate to 0.25 m/s - Change '10' to '>10' to control the speed of acceleration
+        # Slowly Accelrate to 0.25 m/s - Change '10' to 'Value > 10' to control the speed of acceleration
         for step in np.linspace(0, 0.25, 10):
             print("Getting to Speed...")
             cur_motor_command.trans_v = step
@@ -105,7 +106,7 @@ def backward(cur_motor_command, left_hand_gesture):
         if cur_motor_command.trans_v < 0.0:
             return State.BACKWARD
         
-        # Slowly Decelerate to -0.25 m/s - Change '10' to '>10' to control the speed of deceleration
+        # Slowly Decelerate to -0.25 m/s - Change '10' to 'Value > 10' to control the speed of deceleration
         for step in np.linspace(0, 0.25, 10):
             cur_motor_command.trans_v = -step
 
@@ -127,7 +128,7 @@ def left(cur_motor_command, left_hand_gesture):
         if cur_motor_command.angular_v > 0.0:
             return State.LEFT
 
-        # Slowly Decelerate to 0.0 m/s - Change '10' to '>10' to control the speed of deceleration
+        # Slowly Decelerate to 0.0 m/s - Change '10' to 'Value > 10' to control the speed of deceleration
         for step in np.linspace(0, 0.5, 10):
             cur_motor_command.angular_v += step
 
@@ -149,7 +150,7 @@ def right(cur_motor_command, left_hand_gesture):
         if cur_motor_command.angular_v < 0.0:
             return State.RIGHT
 
-        # Slowly Decelerate to 0.0 m/s - Change '10' to '>10' to control the speed of deceleration
+        # Slowly Decelerate to 0.0 m/s - Change '10' to 'Value > 10' to control the speed of deceleration
         for step in np.linspace(0, 0.5, 10):
             cur_motor_command.angular_v -= step
 
@@ -158,103 +159,27 @@ def right(cur_motor_command, left_hand_gesture):
 
 # Claw Close - closes the claw
 def claw_close(cur_motor_command, pwm):
+    # Stop the MBot
     print("Setting speed to 0.0 m/s and 0.0 rad/s")
     cur_motor_command.trans_v = 0.0
     cur_motor_command.angular_v = 0.0
 
-    # GPIO.setmode(GPIO.BOARD)
-    # GPIO.setup(11, GPIO.OUT)
-
-    # # turns on the pin for output
-    # GPIO.output(11, True)
-    # pwm=GPIO.PWM(11, 400)
-
+    # Close Claw
     print("Closing Claw...")
+    pwm.ChangeDutyCycle(40)
 
-    # print("Reseting angle...")
-    # # start it with 0 duty cycle so it doesn't set any angles on startup
-    # pwm.start(20)
-    # sleep(6)
-
-    try:
-        print("set angle to: 90 degrees")
-        # sets an duty cycle approximately to 90 degrees
-        duty = 40
-        # changes the duty cycle to match what we calculated
-        pwm.ChangeDutyCycle(duty)
-        sleep(5)
-    except:
-        print("ERROR: in setting duty cycle")
-    finally:
-        pwm.stop()
-        GPIO.cleanup()
-
-    return State.STAND_BY
+    return State.CLAW_CLOSE
 
 # Claw Open - opens the claw
 def claw_open(cur_motor_command, pwm):
+     # Stop the MBot
     print("Setting speed to 0.0 m/s and 0.0 rad/s")
     cur_motor_command.trans_v = 0.0
     cur_motor_command.angular_v = 0.0
 
-    # GPIO.setmode(GPIO.BOARD)
-    # GPIO.setup(11, GPIO.OUT)
+    # Close Claw
+    print("Closing Claw...")
+    pwm.ChangeDutyCycle(20)
 
-    # # turns on the pin for output
-    # GPIO.output(11, True)
-    # pwm=GPIO.PWM(11, 400)
+    return State.CLAW_OPEN
 
-    print("Opening Claw...")
-    
-    try:
-        print("set angle to: 0 degrees")
-        # sets a variable equal to our angle divided by 18 and 2 added
-        duty = 20
-        # changes the duty cycle to match what we calculated
-        pwm.ChangeDutyCycle(duty)
-        sleep(5)
-    except:
-        print("ERROR: in seeting duty cycle")
-    finally:
-        pwm.stop()
-        GPIO.cleanup()
-
-    print("moved to 0 degrees")
-
-    return State.STAND_BY
-
-'''
-
-# Claw Move - either opens or closes the claw
-def claw_move(cur_motor_command, pwm, Claw_is_Closed):
-    print("Setting speed to 0.0 m/s and 0.0 rad/s")
-    cur_motor_command.trans_v = 0.0
-    cur_motor_command.angular_v = 0.0
-
-    if Claw_is_Closed:
-        print("set angle to: 0 degrees")
-        # sets a variable equal to our angle divided by 18 and 2 added
-        duty = 20
-    else:
-        print("Reseting angle...")
-        # start it with 0 duty cycle so it doesn't set any angles on startup
-        pwm.start(20)
-        sleep(6)
-        
-        print("set angle to: 90 degrees")
-        # sets an duty cycle approximately to 90 degrees
-        duty = 48
-
-    try:
-        # turns on the pin for output
-        GPIO.output(11, True)
-        # changes the duty cycle to match what we calculated
-        pwm.ChangeDutyCycle(duty)
-        sleep(5)
-    finally:
-        pwm.stop()
-        GPIO.cleanup()
-
-    return State.STAND_BY
-
-'''
