1741ee571af9d7a2310d17af7ec0572828c4da2f
c-pg-vega <chrisv99.10@gmail.com>
2024-04-25 00:20:12 -0400

    Added Autonmous to State Machine

--- teleop_gesture/python/teleop_gesture_v3.py
+++ teleop_gesture/python/teleop_gesture_v3.py
@@ -1,15 +1,11 @@
 import lcm
-import numpy as np
-import pygame
 from lcmtypes import mbot_motor_command_t
-from lcmtypes import mbot_encoder_t
 import time
 import sys
-import os
 import RPi.GPIO as GPIO
 from state_functions import *
 import socket
-from flask import Flask, request, jsonify
+import os
 
 def main():
 
@@ -35,23 +31,15 @@ def main():
     pwm = GPIO.PWM(11, 400)
     pwm.start(25)
 
-    # # GPIO -> Output
-    # GPIO.output(11, True)
+    # --- --- Claw initialization End --- --- 
 
-    # # Reset to Open
-    # pwm.ChangeDutyCycle(20)
-
-    # # Clean Up
-    # pwm.stop()
-    # GPIO.cleanup()
-
-    # --- --- Claw initialization End--- --- 
+    # --- --- Socket initialization Start --- --- 
 
     # Raspberry Pi IP address and port
     hostname = socket.gethostname()
     IP = socket.gethostbyname(hostname)
     # TODO: Replace host IP with MBot's if MBot IP changed (use ifconfig and use the wlan0 IP address)
-    server_ip = '192.168.1.2'  # Listen on localhost
+    server_ip = '192.168.1.3'  # Listen on localhost
     server_port = 12345  # Choose a port that is not already in use
 
     # Create a TCP/IP socket
@@ -70,8 +58,18 @@ def main():
 
     print("Connected to:", client_address)
 
+    # --- --- Socket initialization End --- --- 
+
+    # Default settings are Stop and Forward
     left_hand_gesture= "Stop"
     right_hand_gesture = "Forward"
+
+    #  --- --- Clear data.txt so no remaining values exist --- --- 
+    with open('data.txt','w'):
+        pass
+    
+     #  --- --- FLIPPER for alternating angle correction and distance --- --- 
+    angle_distance_flipper = True
 
     # ------------------------------------------------------------
 
@@ -79,14 +77,14 @@ def main():
     lc = lcm.LCM("udpm://239.255.76.67:7667?ttl=1")
     time.sleep(0.5)
 
-    # State Machine
+    # State Machine - Our initial state is always StandBy
     current_state = State.STAND_BY
 
     # Initialize the motor command
     cur_motor_command = mbot_motor_command_t()
 
     while True:
-        # Exit Condition if Ctrl+C is 
+        # Exit Condition if Ctrl+C is pressed
         try:
             # Receive data from the client
             data = client_socket.recv(1024).decode()
@@ -97,55 +95,79 @@ def main():
                 right_hand_gesture = data
             else:
                 None
-            print("Left gesture: ", left_hand_gesture)
-            print("Right gesture: ", right_hand_gesture)
+
+            print("Current Left gesture: ", left_hand_gesture)
+            print("Current Right gesture: ", right_hand_gesture)
 
         except:
             print("Data not received...")
             sys.exit()
 
-        # State Machine
-        if right_hand_gesture == "Forward":
+        # --- --- State Machine --- --- 
+        print("CURRENT STATE: ", current_state)
+        # --- --- Autonomous --- --- 
+
+        # Check if the data.txt file is not empty
+        if os.stat("data.txt").st_size != 0:
+            # This means we have clicked the april tag
+            ### WHEN IN AUTO PLEASE BLINK YOUR LEFT HAND IN GESTURE CAMERA ###
+            print("AUTO MODE ON")
+            current_state = State.AUTO
+        
+        else:
+            current_state = claw_open(cur_motor_command, pwm)
+            current_state = claw_close(cur_motor_command, pwm)
+            current_state = State.STAND_BY
+
+
+        if current_state == State.AUTO:
+            # Run the entire autonomous part of picking up the object
+            # We will only be at this point if we have clicked on an april tag
+            if angle_distance_flipper:
+                current_state = angle_alignment(cur_motor_command)
+                angle_distance_flipper = False
+            
+            else:
+                current_state = distance_alignment(cur_motor_command)
+                angle_distance_flipper = True
+
+        # --- --- Right Hand --- --- 
+
+        if right_hand_gesture == "Forward" and current_state != State.AUTO:
             current_state = forward(cur_motor_command, left_hand_gesture)
             print("State: ", current_state)
 
-        elif right_hand_gesture == "Backwards":
+        elif right_hand_gesture == "Backwards" and current_state != State.AUTO:
             current_state = backward(cur_motor_command, left_hand_gesture)
             print("State: ", current_state)
 
-        elif right_hand_gesture == "TurnLeft":
+        elif right_hand_gesture == "TurnLeft" and current_state != State.AUTO:
             current_state = left(cur_motor_command, left_hand_gesture)
             print("State: ", current_state)
 
-        elif right_hand_gesture == "TurnRight":
+        elif right_hand_gesture == "TurnRight" and current_state != State.AUTO:
             current_state = right(cur_motor_command, left_hand_gesture)
             print("State: ", current_state)
 
-            '''
-            These two states are for the claw control assuming there are two gestures
-            for opening and closing the claw. The claw will only close if it is open
-            and vice versa. However, it would be easy to convert this into one state
-            instead of two with a simple if statement checking the current angle of
-            the claw. It would depend on whether we want the user to use separate gestures
-            or not. Until we can test, it is up to personal preference. 
+        # --- --- Claw --- --- 
 
-            I even wrote the function if we want to switch over called claw_move
-            '''
-        elif right_hand_gesture == "Close" and Claw_is_Closed == False:
+        elif right_hand_gesture == "Close" and Claw_is_Closed == False and current_state != State.AUTO:
             current_state = claw_close(cur_motor_command, pwm)
             print("State: ", current_state)
 
             # Update the claw flag
             Claw_is_Closed = True
         
-        elif right_hand_gesture == "Open" and Claw_is_Closed == True:
+        elif right_hand_gesture == "Open" and Claw_is_Closed == True and current_state != State.AUTO:
             current_state = claw_open(cur_motor_command, pwm)
             print("State: ", current_state)
 
             # Update the claw flag
             Claw_is_Closed = False
 
-        # Publish the motor command
+        # --- --- End of State Machine --- --- 
+
+        # Publish the current motor command
         lc.publish("MBOT_MOTOR_COMMAND", cur_motor_command.encode())
 
         # Sleep for 0.1 seconds
